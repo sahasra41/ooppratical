c1523aed5f41b94dae714462a26886dfd6c0ae0c
sahasra41 <pagadalagodasahasra@gmail.com>
2025-08-02 11:09:39 +0530

    Add files via upload

---
+++ SearchIndex.java
@@ -0,0 +1,15 @@
+//earchIndex
+package oop_pratical;
+
+
+public class SearchIndex{
+	
+	public static void main(String[] args) {
+ String s = "Hello_world";
+        System.out.println(s.indexOf("_"));        
+        System.out.println(s.lastIndexOf("d"));    
+        System.out.println(s.contains("11"));      
+    
+ } 
+ }
+
