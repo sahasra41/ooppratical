9d8d758c5f88438461fce64d64fb97242c643832
sahasra41 <pagadalagodasahasra@gmail.com>
2025-08-23 08:52:54 +0530

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,19 @@
+package oop_practical;
+class Programmer{
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name =  name;
+	}
+}
+	public class Encapsulation{
+		public static void main(String[] args) {
+			Programmer p = new Programmer();
+			p.setName("Geek");
+			System.out.println("Name=> " + p.getName());
+		}
+	}
+
+	
